6a875fec3d44abca7339e451d5bbccbb1fb5d753
jpachter <jpachter@ebay.com>
2014-10-24 16:35:38 -0400

    Setup first cities
    - Rotates through 10 items per city
    - Item image, bid count, price displayed on dialog

--- public/javascripts/map.js
+++ public/javascripts/map.js
@@ -1,51 +1,130 @@
 (function($){
 	var geocoder = new google.maps.Geocoder(),
+		geoCoderRequestInterval = 800,
+		numGeocoded = 0,
 		map,
-		// TODO: throw into a config file
-		cities = [
-			{
-				"name": "Las Vegas, NV",
-				"zoom": 8,
-			}, 
-			{
-				"name": "Miami, FL",
-				"zoom": 7
-			},
-			{
-				"name": "Paris, FR",
-				"zoom": 10
-			},
-			{
-				"name": "New York, NY",
-				"zoom": 12
-			}
-		],
-		panInterval = 6000;
+		panInterval = 6500,
+		areaCount = 0,
+		itemStartDelay = 8000,
+		$window = $(window);
 
-	function updateCity(city, lat, lon) {
-	   for (var i in cities) {
-	     if (cities[i].name == city.name) {
-	        cities[i].lat = lat;
-	        cities[i].lon = lon;
-	        break; //Stop this loop, we found it!
-	     }
-	   }
+	function populateAllCoordinates() {
+		var count = 0;
+		$('.point, .area').each(function() {
+			var $this = $(this);
+			window.setTimeout(function() {
+				setCoordinatesFromPoint($this);
+			}, count++ * geoCoderRequestInterval);
+		});
 	};
 
-	function populateAllCityCoordinates() {
-		for (var i in cities) {
-			setCoordinates(cities[i]);
+	function setCoordinatesFromPoint($point) {
+		if ($point.parent().hasClass('berlin')) {
+			var parentCountry = ' Germany';
 		}
-	};
-	
-	function setCoordinates(city) {
-		geocoder.geocode( { 'address': city.name}, function(results, status) {
+		if ($point.parent().hasClass('sydney')) {
+			var parentCountry = ' Australia';
+		}
+		geocoder.geocode( { 'address': '' + $point.data('postal') + (parentCountry ? parentCountry : '')}, function(results, status) {
 			if (status == google.maps.GeocoderStatus.OK) {
-				updateCity(city, results[0].geometry.location.k, results[0].geometry.location.B);
-			} else {
-			  alert('Geocode was not successful for the following reason: ' + status);
+				$point.data('lat', results[0].geometry.location.k);
+				$point.data('lon', results[0].geometry.location.B);
+				numGeocoded++;
+				console.log('Geocode success!');
+			} else if (status === google.maps.GeocoderStatus.OVER_QUERY_LIMIT) {    
+			  	console.log('Geocode was not successful for the following reason: ' + status + '. Retrying after ' + geoCoderRequestInterval + ' ms.');
+			  	window.setTimeout(function() {
+			  		setCoordinatesFromPoint($point);
+			  	}, geoCoderRequestInterval);
+	        } else {
+			  console.log('Geocode was not successful for the following reason: ' + status);
+			  numGeocoded++;
 			}
 		});
+	};
+
+	function panToCity() {
+    	// Find next city to pan to
+    	var $areas = $('.area'),
+    		$nextArea = $($areas.get(areaCount));
+
+    	if (++areaCount == $areas.length) {
+    		areaCount = 0;
+    	}
+		easey().map(map)
+			.to(map.locationCoordinate({lat: $nextArea.data('lat'), lon: $nextArea.data('lon')}))
+			.zoom($nextArea.data('zoom'))
+			.optimal();
+
+		// Give the map some time to ease to the new location before we move through the items.
+		window.setTimeout(function() {
+			moveThroughItems($nextArea, 0);
+		}, itemStartDelay);
+	 };
+
+	 function moveThroughItems($area, level) {
+		var $points = $(".point", $area);
+
+		// We're done moving through all points! Go to the next city.
+		if ($points.length <= level) {
+			panToCity();
+			return;
+		}
+
+		var $currentPoint = $($points.get(level)),
+			screenPoint = map.locationPoint(new MM.Location($currentPoint.data('lat'), $currentPoint.data('lon')));
+		$currentPoint.css('left', screenPoint.x - 12.5);
+		$currentPoint.css('top', screenPoint.y - 12.5);
+
+		// If element is off screen, skip it.
+		if ($currentPoint.position().left < 0 || $currentPoint.position().left > $window.width()
+			|| $currentPoint.position().top < 0 || $currentPoint.position().top > $window.height()) {
+			moveThroughItems($area, level+1);
+			return;
+		}
+
+		var $infoModule = $('.info-module'),
+			$infoImageContainer = $('.image-container', $infoModule),
+			$infoText = $('p', $infoModule);
+
+		$infoImageContainer.html('<img src=\'' + $currentPoint.data('imageUrl') + '\'>')
+		$infoImageContainer.imagefill();
+		// Imagefill shows some jerkiness so push the fadein back a bit
+		window.setTimeout(function() {
+			$infoModule.animate({
+				opacity: 1
+			}, 200, 'linear');
+
+			$currentPoint.animate({
+				width: 400,
+				height: 400,
+				opacity: .4,
+				left: $currentPoint.position().left - 200,
+				top: $currentPoint.position().top - 200
+			}, 700, 'easeOutQuint', function() {
+
+				var transitionTime = Math.floor(Math.random()*2500 + 2500);
+				$currentPoint.animate({
+					width: 25,
+					height: 25,
+					opacity: 1,
+					left: $currentPoint.position().left + 200,
+					top: $currentPoint.position().top + 200
+				}, 500, 'easeInQuint');
+				$currentPoint.animate({
+					opacity: 0
+				}, transitionTime, 'linear');
+				$infoModule.animate( {
+					opacity: 0
+				}, transitionTime, 'linear');
+				window.setTimeout(function() {
+					moveThroughItems($area, level+1);
+				}, transitionTime);
+		 	});
+		}, 200);
+			
+		$infoText.html($currentPoint.data('bidCount') + ' bids<br>' +
+			$currentPoint.data('currencyId') + ' ' + $currentPoint.data('currentPrice'));
 	};
 
 	$(document).ready(function() {
@@ -53,17 +132,12 @@
 		var subdomains = [ 'a.', 'b.' ];
 		var provider = new MM.TemplatedLayer(template, subdomains);
 		map = new MM.Map('map', provider);
-	  
-	    window.setInterval(function() {
-	    	var randomCity = cities[Math.floor((Math.random() * 4))];
-			easey().map(map)
-				.to(map.locationCoordinate({lat: randomCity.lat, lon: randomCity.lon}))
-				.zoom(randomCity.zoom)
-				.optimal();
-	    }, panInterval);
-
+		populateAllCoordinates();
+		var startTimer = window.setInterval(function() {
+			if (numGeocoded >= $('.area').first().children().length) {
+				panToCity();
+				window.clearInterval(startTimer);
+			}
+		}, 1000);
 	});
-
-	populateAllCityCoordinates();
-
 })(jQuery);
--- routes/map.js
+++ routes/map.js
@@ -1,7 +1,148 @@
+var request = require('request'),
+	step = require('step'),
+	extend = require('util')._extend,
+	protein = require('../public/resources/protein.json'),
+	cities = require('../public/resources/cities.json');
+
 module.exports.init = function(app){
 	app.get('/', buildMap);
 }
 
 function buildMap(req, res){
-	res.render('map');
+	
+	// Configure the request
+	var options = {
+	    method: 'GET',
+	    json: true
+	}
+
+	var worldOptions = extend({}, options);
+	worldOptions.url = 'http://localhost:8080/feedservice/visualization?keyword=new&num=10&algo=highbids';
+
+	var nycOptions = extend({}, options);
+	nycOptions.url = 'http://localhost:8080/feedservice/visualization?keyword=new&zip=10016&distance=30&num=10&algo=highbids';
+
+	var miamiOptions = extend({}, options);
+	miamiOptions.url = 'http://localhost:8080/feedservice/visualization?keyword=new&zip=33030&distance=60&num=10&algo=highbids';
+
+	var londonOptions = extend({}, options);
+	londonOptions.url = 'http://localhost:8080/feedservice/visualization?keyword=new&zip=WC2E7NA&distance=60&num=10&algo=highbids';
+	londonOptions.headers = {};
+    londonOptions.headers["X-EBAY-REST-SITEID"] = 3;
+    londonOptions.headers["X-EBAY-FISNG-GEOINFO"] = "EBAY-GB,en-GB_GB,GBP";
+
+	var berlinOptions = extend({}, options);
+	berlinOptions.url = 'http://localhost:8080/feedservice/visualization?keyword=neu&zip=10178&distance=45&num=10&algo=highbids';
+	berlinOptions.headers = {};
+    berlinOptions.headers["X-EBAY-REST-SITEID"] = 77;
+    berlinOptions.headers["X-EBAY-FISNG-GEOINFO"] = "EBAY-DE,de-DE_DE,EUR";
+	
+	var sydneyOptions = extend({}, options);
+	sydneyOptions.url = 'http://localhost:8080/feedservice/visualization?keyword=new&zip=2000&distance=30&num=10&algo=highbids';
+	sydneyOptions.headers = {};
+    sydneyOptions.headers["X-EBAY-REST-SITEID"] = 15;
+    sydneyOptions.headers["X-EBAY-FISNG-GEOINFO"] = "EBAY-AU,en-AU_AU,AUD";
+
+	var items = {};
+
+	step(
+		function getCollections() {
+		    request(worldOptions, this.parallel());
+		    request(nycOptions, this.parallel());
+		    request(miamiOptions, this.parallel());
+		    request(londonOptions, this.parallel());
+		    request(berlinOptions, this.parallel());
+		    request(sydneyOptions, this.parallel());
+		},
+		function renderPage(err, world, nyc, miami, london, berlin, sydney) {
+		    if (!err && world.body) {
+                if (world.body && world.body.ack == 'SUCCESS') {
+                	items.world = [];
+                	for (var i in world.body.items) {
+                		if (world.body.items[i].postal) {
+							items.world.push(world.body.items[i]);
+                		}
+                	}
+                } else {
+                	console.log(err);
+                    //TODO: error handling
+	        	}
+	        }
+
+	        if (!err && nyc.body) {
+                if (nyc.body && nyc.body.ack == 'SUCCESS') {
+                	items.nyc = [];
+                	for (var i in nyc.body.items) {
+                		if (nyc.body.items[i].postal) {
+                			items.nyc.push(nyc.body.items[i]);
+                		}
+                	}
+                } else {
+                	console.log(err);
+                    //TODO: error handling
+	        	}
+	    	}
+
+	        if (!err && miami.body) {
+                if (miami.body && miami.body.ack == 'SUCCESS') {
+                	items.miami = [];
+                	for (var i in miami.body.items) {
+                		if (miami.body.items[i].postal) {
+							items.miami.push(miami.body.items[i]);
+						}
+                	}
+                } else {
+                	console.log(err);
+                    //TODO: error handling
+	        	}
+	    	}
+
+	        if (!err && london.body) {
+                if (london.body && london.body.ack == 'SUCCESS') {
+                	items.london = [];
+                	for (var i in london.body.items) {
+                		if (london.body.items[i].postal) {
+                			items.london.push(london.body.items[i]);
+                		}
+                	}
+                } else {
+                	console.log(err);
+                    //TODO: error handling
+	        	}
+	    	}
+
+	        if (!err && berlin.body) {
+                if (berlin.body && berlin.body.ack == 'SUCCESS') {
+                	items.berlin = [];
+                	for (var i in berlin.body.items) {
+                		if (berlin.body.items[i].postal) {
+                			items.berlin.push(berlin.body.items[i]);
+                		}
+                	}
+                } else {
+                	console.log(err);
+                    //TODO: error handling
+	        	}
+	    	}
+
+	    	if (!err && sydney.body) {
+                if (sydney.body && sydney.body.ack == 'SUCCESS') {
+                	items.sydney = [];
+                	for (var i in sydney.body.items) {
+                		if (sydney.body.items[i].postal) {
+                			items.sydney.push(sydney.body.items[i]);
+                		}
+                	}
+                } else {
+                	console.log(err);
+                    //TODO: error handling
+	        	}
+	    	}
+
+	    	res.render('map', {
+				items: items,
+				cities: cities
+			});
+	    }
+	)
 };
